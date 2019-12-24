737864f194d45823b9570ee355854696b4161d70
Lam Gia Thuan <duckladydinh@gmail.com>
2019-12-24 05:02:00 +0100

    na: test pre-commit-hook

--- ktlint-pre-commit-hook.sh
+++ ktlint-pre-commit-hook.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # https://github.com/shyiko/ktlint pre-commit hook
-pwd
 echo Reformatting code...
-git diff --name-only --cached --relative | grep '\.kt[s"]\?$' | xargs ./ktlint -F --relative . --disabled_rules=no-wildcard-imports,experimental:annotation
-if [ $? -ne 0 ]; then exit 1; fi
+git diff --name-only --relative | grep '\.kt[s"]\?$' | xargs ./ktlint -F --relative . --disabled_rules=no-wildcard-imports,experimental:annotation
+echo Code reformatted!
--- src/test/kotlin/thuan/handsome/lbfgsb/LBFGSBTest.kt
+++ src/test/kotlin/thuan/handsome/lbfgsb/LBFGSBTest.kt
@@ -1,50 +1,45 @@
 package thuan.handsome.lbfgsb
 
-import org.junit.Test
-import thuan.handsome.utils.LOGGER
 import kotlin.math.pow
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import org.junit.Test
 
 class LBFGSBTest {
-	@Test
-	fun testUnboundedRosen() {
-		val rosen = { params: DoubleArray ->
-			val n = params.size
-			var res = 0.0
-			for (i in 1 until n) {
-				res += 100.0 * (params[i] - params[i - 1].pow(2)).pow(2) + (1 - params[i - 1]).pow(2)
-			}
+    @Test
+    fun testUnboundedRosen() {
+        val rosen = { params: DoubleArray ->
+            val n = params.size
+            var res = 0.0
+            for (i in 1 until n) {
+                res += 100.0 * (params[i] - params[i - 1].pow(2)).pow(2) + (1 - params[i - 1]).pow(2)
+            }
 
-			res
-		}
-		assertEquals(76.56, rosen.invoke((0 until 10).map { it * 0.1 }.toDoubleArray()))
+            res
+        }
+        assertEquals(76.56, rosen.invoke((0 until 10).map { it * 0.1 }.toDoubleArray()))
 
-		val res = LBFGSBOptimizer.minimize(rosen, doubleArrayOf(1.3, 0.7, 0.8, 1.9, 1.2))
-		assertTrue(res.x.map { (it - 1).pow(2) }.sum() < 0.001)
-		assertTrue(res.y < 0.001)
+        val res = LBFGSBOptimizer.minimize(rosen, doubleArrayOf(1.3, 0.7, 0.8, 1.9, 1.2))
+        assertTrue(res.x.map { (it - 1).pow(2) }.sum() < 0.001)
+        assertTrue(res.y < 0.001)
+    }
 
-        // LOGGER.info { "$res" }
-	}
+    @Test
+    fun testBoundedFunction() {
+        val f = { params: DoubleArray ->
+            val x = params[0]
+            val y = params[1]
+            val z = params[2]
+            x * x - y * y * z
+        }
 
-	@Test
-	fun testBoundedFunction() {
-		val f = { params: DoubleArray ->
-			val x = params[0]
-			val y = params[1]
-			val z = params[2]
-			x * x - y * y * z
-		}
+        val res = LBFGSBOptimizer.minimize(
+            f, doubleArrayOf(1.0, 1.0, 0.0), listOf(
+                Bound(1.0, 5.0), Bound(-2.0, 3.0), Bound(-5.0, 1.0)
+            )
+        )
 
-		val res = LBFGSBOptimizer.minimize(
-			f, doubleArrayOf(1.0, 1.0, 0.0), listOf(
-				Bound(1.0, 5.0), Bound(-2.0, 3.0), Bound(-5.0, 1.0)
-			)
-		)
-
-		assertEquals(doubleArrayOf(1.0, 3.0, 1.0).toList(), res.x.toList())
-		assertEquals(-8.0, res.y)
-
-        // LOGGER.info { "$res" }
-	}
+        assertEquals(doubleArrayOf(1.0, 3.0, 1.0).toList(), res.x.toList())
+        assertEquals(-8.0, res.y)
+    }
 }
